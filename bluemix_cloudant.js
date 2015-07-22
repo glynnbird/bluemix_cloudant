76e4415449eb1cc691fe88a1734425873fd005ce
Glynn Bird <glynn.bird@gmail.com>
2015-07-22 13:23:17 +0100

    upgrade to cloudant instead of nano

--- bluemix_cloudant.js
+++ bluemix_cloudant.js
@@ -2,12 +2,12 @@ var url = "http://127.0.0.1:5984/";
 
 // parse BlueMix  configuration from environment variables, if present
 var services = process.env.VCAP_SERVICES
-if(typeof services != 'undefined') {
+if (typeof services != 'undefined') {
   services = JSON.parse(services);
   url = services.cloudantNoSQLDB[0].credentials.url
 }
 
 // start nano with the url of Cloudant or local CouchDB
-var nano = require('nano')(url);
+var cloudant = require('cloudant')(url);
 
-module.exports = nano;
+module.exports = cloudant;
